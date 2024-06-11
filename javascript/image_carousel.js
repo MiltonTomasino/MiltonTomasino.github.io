a3e9071119bc1e242253963049d9e2f84fc08ba0
MiltonTomasino <jonathantomasino13@gmail.com>
2024-06-10 21:56:51 -0700

    Changed structure to be more modular

---
+++ javascript/image_carousel.js
@@ -0,0 +1,26 @@
+let slideIndices = [1, 1];
+
+showSlides(1, 'C1', 0);
+showSlides(1, 'C2', 1);
+
+function plusSlides(n, carouselClass, carouselIndex) {
+    showSlides(slideIndices[carouselIndex] += n, carouselClass, carouselIndex);
+}
+
+function showSlides(n, carouselClass, carouselIndex) {
+    let i;
+    let slides = document.getElementsByClassName(carouselClass);
+
+    if (n > slides.length) {
+        slideIndices[carouselIndex] = 1;
+    }
+    if (n < 1) {
+        slideIndices[carouselIndex] = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndices[carouselIndex] - 1].style.display = "block";
+}
